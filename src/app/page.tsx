12595212be3cedc34160f3def54bcef2a3fc154e
“nickyleech” <nicky.leech@pa.media>
2025-06-27 23:27:08 +0100

    Fix Tailwind v3 setup - app working perfectly locally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import HandoverApp from '../components/HandoverApp';
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Test Page</h1>
-      <p>If you can see this, the routing works!</p>
-    </div>
-  );
+  return <HandoverApp />;
 }
